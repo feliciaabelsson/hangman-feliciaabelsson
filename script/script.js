70d6f32c94fe6fb31b2fc79a0bdf71b2691e0759
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2020-11-02 10:35:18 +0100

    alphabet function Eva

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 //----Variabler
 //let words = lista med ord
 //let alphabet = 'a','b','c';
+window.onload= function(){
+    var alphabet=[ 'a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+}
 
 //----Functioner
 //function på vad som händer om en bokstav klickas på 

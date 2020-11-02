4731bea65d22db28fdfb8f666d77c4cbb66b0348
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2020-11-02 17:29:01 +0100

    change

--- script/script.js
+++ script/script.js
@@ -13,8 +13,22 @@ var showlives=
 document.getElementById("mylives");
 var showClue= document.getElementById("clue");
 
-//the words that will be randomized 
-let words = ['ord1','ord2','ord3','ord4'];
+//the words that will be randomized // Create an array of words
+var words = [
+    "javascript",
+    "monkey",
+    "amazing",
+    "pancake",
+    "galvainze",
+    "cohort",
+    "concatenate",
+    "iteration",
+    "index",
+    "code",
+    "angular",
+    "react",
+    "python"
+    ];
 
 //let words = lista med ord
 
@@ -95,5 +109,143 @@ Comment=function(){
         }
         
     }
+      
+  
+
+      // Animate man
+      var animate = function () {
+        var drawMe = lives ;
+        drawArray[drawMe]();
+      }
+    
+      
+       // Hangman
+      canvas =  function(){
+    
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.strokeStyle = "#fff";
+        context.lineWidth = 2;
+      };
+      
+        head = function(){
+          myStickman = document.getElementById("stickman");
+          context = myStickman.getContext('2d');
+          context.beginPath();
+          context.arc(60, 25, 10, 0, Math.PI*2, true);
+          context.stroke();
+        }
+        
+      draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+        
+        context.moveTo($pathFromx, $pathFromy);
+        context.lineTo($pathTox, $pathToy);
+        context.stroke(); 
+    }
+    
+       frame1 = function() {
+         draw (0, 150, 150, 150);
+       };
+       
+       frame2 = function() {
+         draw (10, 0, 10, 600);
+       };
+      
+       frame3 = function() {
+         draw (0, 5, 70, 5);
+       };
+      
+       frame4 = function() {
+         draw (60, 5, 60, 15);
+       };
+      
+       torso = function() {
+         draw (60, 36, 60, 70);
+       };
+      
+       rightArm = function() {
+         draw (60, 46, 100, 50);
+       };
+      
+       leftArm = function() {
+         draw (60, 46, 20, 50);
+       };
+      
+       rightLeg = function() {
+         draw (60, 70, 100, 100);
+       };
+      
+       leftLeg = function() {
+         draw (60, 70, 20, 100);
+       };
+      
+      drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+    
+    
+      // OnClick Function
+       check = function () {
+        list.onclick = function () {
+          var geuss = (this.innerHTML);
+          this.setAttribute("class", "active");
+          this.onclick = null;
+          for (var i = 0; i < word.length; i++) {
+            if (word[i] === geuss) {
+              geusses[i].innerHTML = geuss;
+              counter += 1;
+            } 
+          }
+          var j = (word.indexOf(geuss));
+          if (j === -1) {
+            lives -= 1;
+            comments();
+            animate();
+          } else {
+            comments();
+          }
+        }
+      }
+      
+     
+  // Play
+  play = function () {  
+    var words = [
+        "javascript",
+        "monkey",
+        "amazing",
+        "pancake",
+        "galvainze",
+        "cohort",
+        "concatenate",
+        "iteration",
+        "index",
+        "code",
+        "angular",
+        "react",
+        "python"
+        ];
+        
+// Pick a random word
+var word = words[Math.floor(Math.random() * words.length)];
+
+// Set up the amswer answerArrayvar answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+
+        geusses = [ ];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        result();
+        comments();
+        selectCat();
+        canvas();
+      }
+    
+      play();
+            
 }
 //En eventlistener för knapptryck på bokstav som går in i funktion x 

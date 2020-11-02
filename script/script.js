f3b57db8c75c079db1fa55282b675a1bca52b19d
Felicia Abelsson <f.abelsson@live.se>
2020-11-02 11:20:39 +0100

    added in script + added letters container in html

--- script/script.js
+++ script/script.js
@@ -1,14 +1,31 @@
-//----Variabler
-//let words = lista med ord
-//let alphabet = 'a','b','c';
+//----Variables
+//the words that will be randomized 
+let words = ['ord1','ord2','ord3','ord4'];
+
+//the letters that will act as buttons in html
+let lettersContainer = document.querySelector('.letters');
+
+//select letters container in html
 window.onload= function(){
     var alphabet=[ 'a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 }
 
 //----Functioner
 //function på vad som händer om en bokstav klickas på 
-//function randomWord() = gå igenom lista med ord och ge ut ett random 
+//if/else sats vid klick på bokstav 
+//skapa ul lista där bokstäverna som är rätt läggs in i (createElement)
+//i Html ska de tomma bokstäverna för ordet ersättas med bokstäver 
 
+//---Randomizer 
+//function randomWord() = gå igenom lista med ord och ge ut ett random
+//loopa igenom alphabet.length för att kunna matcha 
+
+//----Generate letters 
+//Create a variable that creates span in html 
+//Create variable that creates text node 
+//Append letter as child to span so they connects 
+//Add class on span (.className) so we can change in css 
+//Append span to the letters container in html 
 
 //----Eventlisteners
 //En eventlistener för knapptryck på bokstav som går in i funktion x 

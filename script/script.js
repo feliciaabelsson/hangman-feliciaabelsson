5b3df3eaf6a712ef72ec6d6848caa55be43d02a8
Felicia Abelsson <f.abelsson@live.se>
2020-11-02 10:21:29 +0100

    init start changes

---
+++ script/script.js
@@ -0,0 +1,11 @@
+//----Variabler
+//let words = lista med ord
+//let alphabet = 'a','b','c';
+
+//----Functioner
+//function på vad som händer om en bokstav klickas på 
+//function randomWord() = gå igenom lista med ord och ge ut ett random 
+
+
+//----Eventlisteners
+//En eventlistener för knapptryck på bokstav som går in i funktion x 
